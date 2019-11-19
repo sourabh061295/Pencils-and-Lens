d19291932220182fd76ec2eb05c7e34b015e92c0
Kulkarni Sourabh A R (RBEI/ESX1) <SourabhAR.Kulkarni@in.bosch.com>
2019-11-19 15:14:42 +0530

    Added order page and changed jumboname effects

--- assets/js/galleries.js
+++ assets/js/galleries.js
@@ -19,3 +19,9 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+$(document).keydown(function(event) { 
+  if (event.keyCode == 27) { 
+    modal.style.display = "none";
+  }
+});
