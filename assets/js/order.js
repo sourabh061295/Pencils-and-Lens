46dfa7f997887ced7990a089fa47ec6deb925797
Kulkarni Sourabh A R (RBEI/ESX1) <SourabhAR.Kulkarni@in.bosch.com>
2019-11-22 17:15:04 +0530

    Added quotation form

--- assets/js/order.js
+++ assets/js/order.js
@@ -3,12 +3,11 @@ var img_list = ["../data/images/order_image.jpg","../data/images/sketch.png","..
 var idx = 0;
 
 function changeimg() {
-    idx = idx < img_list.length ? idx: 0;
     $('#imagedisplay').fadeOut('slow', function() {
         $(this).css("background-image", "url("+img_list[idx]+")");  
         $(this).fadeIn('slow');
     });
-    idx++;
+    idx = ((idx+1) % img_list.length);
 }
 
 setInterval(changeimg, 2000);
