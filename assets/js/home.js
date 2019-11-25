2fbbae8c64f40a92217ea640779edd2167828e0b
Kulkarni Sourabh A R (RBEI/ESX1) <SourabhAR.Kulkarni@in.bosch.com>
2019-11-25 17:41:21 +0530

    Design and form tweaks

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,8 +1,21 @@
 //Scroll effect for navbar
 $(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
+    if ($(document).scrollTop() > 50 && $("#jumboName > h1").text()!= " World of Colors!") {
       $('nav').addClass('scrolled');
     } else {
       $('nav').removeClass('scrolled');
     }
 });
+
+//Changing background of jumbotron
+switch (document.querySelector("#jumboName > h1").textContent) {
+  case " Art Collection": document.querySelector(".jumbotron").style.backgroundImage = "url(../data/images/sample.jpg)";
+                          break;
+  case " Image Gallery":  document.querySelector(".jumbotron").style.backgroundImage = "url(../data/images/portrait.png)";
+                          break;
+  case " Order A Sketch": document.querySelector(".jumbotron").style.backgroundImage = "url(../data/images/order_image.jpg)";
+                          break;
+  case " World of Colors!": document.querySelector(".jumbotron").style.backgroundImage = "url(../data/images/sketch.png)";
+                            document.querySelector(".navbar").classList.add("rainbow_background");
+                            break;
+}
