ae7d9ecb7b5376952523162186cff892095988c0
Kulkarni Sourabh A R (RBEI/ESX1) <SourabhAR.Kulkarni@in.bosch.com>
2019-12-02 17:02:20 +0530

    Updated images and minor design tweaks

--- assets/js/home.js
+++ assets/js/home.js
@@ -9,13 +9,13 @@ $(window).scroll(function() {
 
 //Changing background of jumbotron
 switch (document.querySelector("#jumboName > h1").textContent) {
-  case " Art Collection": document.querySelector(".jumbotron").style.backgroundImage = "url(../data/images/inktober.jpg)";
+  case " Art Collection": document.querySelector(".jumbotron").style.backgroundImage = "url(../data/images/frames.jpg)";
                           break;
-  case " Image Gallery":  document.querySelector(".jumbotron").style.backgroundImage = "url(../data/images/frames.jpg)";
+  case " Image Gallery":  document.querySelector(".jumbotron").style.backgroundImage = "url(../data/images/photo_wall.jpg)";
                           break;
   case " Order A Sketch": document.querySelector(".jumbotron").style.backgroundImage = "url(../data/images/sketch_order.jpg)";
                           break;
-  case " World of Colors!": document.querySelector(".jumbotron").style.backgroundImage = "url(../data/images/ronald-cuyan-AJgFLjnmSs4-unsplash.jpg)";
+  case " World of Colors!": document.querySelector(".jumbotron").style.backgroundImage = "url(../data/images/olav-ahrens-rotne-4Ennrbj1svk-unsplash.jpg)";
                             document.querySelector(".navbar").classList.add("rainbow_background");
                             break;
 }
